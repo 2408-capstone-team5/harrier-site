996a40492ee0ff8cf3cfd56a58c61df16a658e38
joelbarton-io <joelbarton406@gmail.com>
2025-01-06 21:04:08 -0700

    chore: updated vite.config, browserRouter, deploy.yaml & package.json
    Co-authored-by: Jesse Kercheval <jessekerch@users.noreply.github.com>
    Co-authored-by: Shane Ziegler <shaneziegler@users.noreply.github.com>
    Co-authored-by: Wook Kim <wook2000@users.noreply.github.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { PageNavigationProvider } from "../src/providers/PageNavigation"; // Import the provider
+import { PageNavigationProvider } from "../src/providers/PageNavigation";
 
 import TryHarrierPage from "./components/pages/TryHarrierPage";
 import Layout from "./components/utility/Layout";
@@ -13,66 +13,71 @@ import Design from "./components/pages/case-study/Design";
 import Implementation from "./components/pages/case-study/Implementation";
 import "./index.css";
 
+const basename = "/harrier-site";
+
 createRoot(document.getElementById("app")!).render(
   <StrictMode>
     <PageNavigationProvider>
       <RouterProvider
-        router={createBrowserRouter([
-          {
-            path: "/",
-            element: <Layout />,
-            errorElement: <div>generic error</div>, // TODO: Add improved error page
-            children: [
-              {
-                index: true,
-                element: <LandingPage />,
-              },
-              {
-                path: "case-study",
-                element: <CaseStudyHomePage />,
-                errorElement: <NotFoundPage />,
-                children: [
-                  {
-                    path: "problem-domain",
-                    element: <ProblemDomain />,
-                  },
-                  {
-                    path: "design",
-                    element: <Design />,
-                  },
-                  {
-                    path: "implementation",
-                    element: <Implementation />,
-                  },
-                  {
-                    path: "future-work",
-                    element: <div>Future Work</div>,
-                  },
-                  {
-                    path: "*",
-                    element: <NotFoundPage />,
-                  },
-                ],
-              },
-              {
-                path: "team",
-                element: <div>team</div>,
-              },
-              {
-                path: "try-harrier",
-                element: (
-                  <Suspense fallback={<div>Loading...</div>}>
-                    <TryHarrierPage />
-                  </Suspense>
-                ),
-              },
-              {
-                path: "*",
-                element: <NotFoundPage />,
-              },
-            ],
-          },
-        ])}
+        router={createBrowserRouter(
+          [
+            {
+              path: "/",
+              element: <Layout />,
+              errorElement: <div>generic error</div>, // TODO: Add improved error page
+              children: [
+                {
+                  index: true,
+                  element: <LandingPage />,
+                },
+                {
+                  path: "case-study",
+                  element: <CaseStudyHomePage />,
+                  errorElement: <NotFoundPage />,
+                  children: [
+                    {
+                      path: "problem-domain",
+                      element: <ProblemDomain />,
+                    },
+                    {
+                      path: "design",
+                      element: <Design />,
+                    },
+                    {
+                      path: "implementation",
+                      element: <Implementation />,
+                    },
+                    {
+                      path: "future-work",
+                      element: <div>Future Work</div>,
+                    },
+                    {
+                      path: "*",
+                      element: <NotFoundPage />,
+                    },
+                  ],
+                },
+                {
+                  path: "team",
+                  element: <div>team</div>,
+                },
+                {
+                  path: "try-harrier",
+                  element: (
+                    <Suspense fallback={<div>Loading...</div>}>
+                      <TryHarrierPage />
+                    </Suspense>
+                  ),
+                },
+                {
+                  path: "*",
+                  element: <NotFoundPage />,
+                },
+              ],
+            },
+          ],
+          { basename },
+        )}
       />
     </PageNavigationProvider>
   </StrictMode>,
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/harrier-site/",
   build: {
     outDir: "dist",
     sourcemap: false,
